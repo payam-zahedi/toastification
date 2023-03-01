09ee489b782baa8c733892cae452ad3227fe9d25
Payam Zahedi <pzahedi1995@gmail.com>
2023-03-01 11:35:00 +0330

    add some todo

--- lib/toastification.dart
+++ lib/toastification.dart
@@ -2,3 +2,8 @@ library toastification;
 
 export 'src/notification/toastification_manager.dart';
 export 'src/notification/toastification_item.dart';
+
+// TODO(payam): samples
+// TODO(payam): progress bar
+// TODO(payam): custom animation
+// TODO(payam): add alignment for 
