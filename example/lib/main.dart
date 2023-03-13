e9bf9df09776f9aa8323974446b4f050720bd972
Payam Zahedi <pzahedi1995@gmail.com>
2023-03-14 00:04:14 +0330

    update example main.dart

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -27,92 +27,88 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.grey,
         title: const Text('Toastification Example'),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Align(
-          alignment: Alignment.bottomCenter,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          alignment: Alignment.bottomLeft,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
             children: <Widget>[
-              Expanded(
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: const Color(0xff1976d2),
-                  ),
-                  onPressed: () {
-                    toastification.show(
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color(0xff1976d2),
+                  minimumSize: const Size(150, 40),
+                ),
+                onPressed: () {
+                  toastification.show(
+                    context: context,
+                    autoCloseDuration: const Duration(seconds: 5),
+                    title: 'Title',
+                  );
+                },
+                child: const Text('Default'),
+              ),
+              const SizedBox(height: 8),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blue,
+                  minimumSize: const Size(150, 40),
+                ),
+                onPressed: () {
+                  toastification.showInfo(
                       context: context,
                       autoCloseDuration: const Duration(seconds: 5),
-                      title: 'Title',
-                    );
-                  },
-                  child: const Text('Default'),
-                ),
+                      title: 'Title');
+                },
+                child: const Text('Info'),
               ),
-              const SizedBox(width: 8),
-              Expanded(
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.blue,
-                  ),
-                  onPressed: () {
-                    toastification.showInfo(
-                        context: context,
-                        autoCloseDuration: const Duration(seconds: 5),
-                        title: 'Title');
-                  },
-                  child: const Text('Info'),
+              const SizedBox(height: 8),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.amberAccent,
+                  minimumSize: const Size(150, 40),
                 ),
+                onPressed: () {
+                  toastification.showWarning(
+                    context: context,
+                    autoCloseDuration: const Duration(seconds: 5),
+                    title: 'Title',
+                  );
+                },
+                child: const Text('Warning'),
               ),
-              const SizedBox(width: 8),
-              Expanded(
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.amberAccent,
-                  ),
-                  onPressed: () {
-                    toastification.showWarning(
-                      context: context,
-                      autoCloseDuration: const Duration(seconds: 5),
-                      title: 'Title',
-                    );
-                  },
-                  child: const Text('Warning'),
+              const SizedBox(height: 8),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.green,
+                  minimumSize: const Size(150, 40),
                 ),
+                onPressed: () {
+                  toastification.showSuccess(
+                    context: context,
+                    autoCloseDuration: const Duration(seconds: 5),
+                    title: 'Title',
+                  );
+                },
+                child: const Text('Success'),
               ),
-              const SizedBox(width: 8),
-              Expanded(
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.green,
-                  ),
-                  onPressed: () {
-                    toastification.showSuccess(
-                      context: context,
-                      autoCloseDuration: const Duration(seconds: 5),
-                      title: 'Title',
-                    );
-                  },
-                  child: const Text('Success'),
+              const SizedBox(height: 8),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.red,
+                  minimumSize: const Size(150, 40),
                 ),
-              ),
-              const SizedBox(width: 8),
-              Expanded(
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.red,
-                  ),
-                  onPressed: () {
-                    toastification.showError(
-                      context: context,
-                      autoCloseDuration: const Duration(seconds: 5),
-                      title: 'Title',
-                    );
-                  },
-                  child: const Text('Error'),
-                ),
+                onPressed: () {
+                  toastification.showError(
+                    context: context,
+                    autoCloseDuration: const Duration(seconds: 5),
+                    title: 'Title',
+                  );
+                },
+                child: const Text('Error'),
               ),
             ],
           ),
