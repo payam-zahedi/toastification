752f9b97dcc2abf8618adad2e1957f56583d47fc
Payam Zahedi <pzahedi1995@gmail.com>
2023-05-22 12:38:31 +0330

    add header to home example

--- example/lib/src/features/home/views/pages/home.dart
+++ example/lib/src/features/home/views/pages/home.dart
@@ -1,6 +1,7 @@
 import 'package:example/src/core/views/widgets/divider.dart';
 import 'package:example/src/features/home/views/widgets/app_bar.dart';
 import 'package:example/src/features/home/views/widgets/customization_panel.dart';
+import 'package:example/src/features/home/views/widgets/header.dart';
 import 'package:example/src/features/home/views/widgets/preview_panel.dart';
 import 'package:flutter/material.dart';
 
@@ -13,6 +14,10 @@ class HomeScreen extends StatelessWidget {
       body: CustomScrollView(
         slivers: [
           SliverToBoxAdapter(child: ToastAppBar()),
+          SliverPadding(
+            padding: EdgeInsets.only(top: 100),
+            sliver: SliverToBoxAdapter(child: ToastHeader()),
+          ),
         ],
       ),
     );
---
+++ example/lib/src/features/home/views/widgets/header.dart
@@ -0,0 +1,65 @@
+
+import 'package:example/src/core/views/widgets/core.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class ToastHeader extends StatelessWidget {
+  const ToastHeader({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
+    return Column(
+      children: [
+        const ColoredTag(
+          text: 'FLUTTER TOAST NOTIFICATIONS',
+        ),
+        const SizedBox(height: 8),
+        Text(
+          'Nostrum sequi beatae\nsed excepturi voly.',
+          style: theme.textTheme.displayLarge?.copyWith(
+            fontSize: 56,
+            fontWeight: FontWeight.w500,
+            color: theme.colorScheme.onBackground,
+          ),
+          textAlign: TextAlign.center,
+        ),
+        const SizedBox(height: 24),
+        Text(
+          'Ullam possimus vitae eos maiores enim y.A sunt ducimus consequuntur\n ducimus qut.In cum esse beatae id laborum dolores l.Commodi at qui\n reiciendis. Eos sunt eot.Maiores sed sequi nulla quas asperiorex.',
+          style: theme.textTheme.bodyLarge?.copyWith(
+            fontSize: 18,
+            fontWeight: FontWeight.w100,
+            color: theme.colorScheme.onBackground,
+          ),
+          textAlign: TextAlign.center,
+        ),
+        const SizedBox(height: 65),
+        Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            FilledButton(
+              style: FilledButton.styleFrom(
+                padding: const EdgeInsets.symmetric(horizontal: 40),
+              ),
+              onPressed: () {},
+              child: const Text('Make a Random Toast'),
+            ),
+            const SizedBox(width: 24),
+            FilledButton.icon(
+              style: FilledButton.styleFrom(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                backgroundColor: theme.colorScheme.secondary,
+                foregroundColor: theme.colorScheme.onSecondary,
+              ),
+              onPressed: () {},
+              label: const Text('Make a Random Toast'),
+              icon: const FaIcon(FontAwesomeIcons.star),
+            )
+          ],
+        )
+      ],
+    );
+  }
+}
