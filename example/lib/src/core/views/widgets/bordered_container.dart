a5d04c4fbb84887852dce516816ecb194d81c953
Payam Zahedi <pzahedi1995@gmail.com>
2023-05-23 14:40:50 +0330

    add bordered container widget

---
+++ example/lib/src/core/views/widgets/bordered_container.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+
+class BorderedContainer extends StatelessWidget {
+  const BorderedContainer({
+    super.key,
+    this.active = false,
+    this.height,
+    this.width,
+    this.elevation = 0,
+    this.margin,
+    this.padding,
+    this.onTap,
+    this.child,
+  });
+
+  final bool active;
+  final double? height;
+  final double? width;
+
+  final double elevation;
+
+  final EdgeInsetsGeometry? padding;
+  final EdgeInsetsGeometry? margin;
+
+  final VoidCallback? onTap;
+
+  final Widget? child;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
+    final backgroundColor = active
+        ? theme.colorScheme.primary.withOpacity(.1)
+        : theme.colorScheme.background;
+
+    final foreground =
+        active ? theme.colorScheme.primary : theme.colorScheme.onBackground;
+
+    final borderColor =
+        active ? theme.colorScheme.primary : theme.colorScheme.outline;
+
+    final borderSize = active ? 1.5 : 1.0;
+    return Padding(
+      padding: margin ?? EdgeInsets.zero,
+      child: InkWell(
+        onTap: onTap,
+        borderRadius: BorderRadius.circular(10),
+        child: SizedBox(
+          height: height ?? 48,
+          width: width,
+          child: Material(
+            animationDuration: const Duration(milliseconds: 400),
+            color: backgroundColor,
+            elevation: elevation,
+            
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+              side: BorderSide(color: borderColor, width: borderSize),
+            ),
+            child: Padding(
+              padding: padding ?? EdgeInsets.zero,
+              child: Center(
+                child: AnimatedDefaultTextStyle(
+                  duration: const Duration(milliseconds: 300),
+                  style: theme.textTheme.titleSmall!.copyWith(
+                    fontSize: 14,
+                    fontWeight: active ? FontWeight.w500 : FontWeight.w400,
+                    color: foreground,
+                  ),
+                  child: IconTheme.merge(
+                    data: IconThemeData(
+                      size: 24,
+                      color: foreground,
+                    ),
+                    child: child ?? const SizedBox(),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
