cbeafc0555fd9d866208ef7c5fe1f8a072127508
Payam Zahedi <pzahedi1995@gmail.com>
2023-05-24 12:33:33 +0330

    add bordered drop down

---
+++ example/lib/src/core/views/widgets/drop_down.dart
@@ -0,0 +1,61 @@
+
+import 'package:example/src/core/views/widgets/soon.dart';
+import 'package:flutter/material.dart';
+
+class BorderedDropDown<T> extends StatelessWidget {
+  const BorderedDropDown({
+    super.key,
+    required this.hint,
+    this.value,
+    required this.items,
+    this.onChanged,
+    this.available = true,
+  });
+
+  final String hint;
+  final T? value;
+  final List<DropdownMenuItem<T>> items;
+  final ValueChanged<T?>? onChanged;
+
+  final bool available;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return DropdownButtonFormField<T>(
+      icon: const Icon(Icons.keyboard_arrow_down),
+      focusColor: theme.colorScheme.background,
+      borderRadius: BorderRadius.circular(10),
+      decoration: InputDecoration(
+        hintText: hint,
+        hintStyle: theme.textTheme.bodyMedium?.copyWith(
+          height: 1.1,
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+          borderSide: BorderSide(
+            color: theme.colorScheme.outline,
+          ),
+        ),
+        filled: false,
+      ),
+      hint: !available
+          ? Row(
+              children: [
+                Text(
+                  hint,
+                  style: theme.textTheme.bodyMedium?.copyWith(
+                    height: 1.1,
+                  ),
+                ),
+                const SizedBox(width: 4),
+                const SoonWidget(),
+              ],
+            )
+          : null,
+      value: value,
+      items: items,
+      onChanged: available ? onChanged : null,
+    );
+  }
+}
