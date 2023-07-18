a8100e3d663e713e1dfff23839d615629b374508
Payam Zahedi <pzahedi1995@gmail.com>
2023-07-18 19:43:31 +0330

    add animation types

---
+++ example/lib/src/features/home/views/ui_states/animation_type.dart
@@ -0,0 +1,119 @@
+import 'package:flutter/material.dart';
+import 'package:toastification/toastification.dart';
+
+sealed class AnimationType {
+  const AnimationType({
+    required this.title,
+    required this.name,
+  });
+
+  final String title;
+  final String name;
+
+  String buildCode();
+
+  Widget builder(
+    BuildContext context,
+    Animation<double> animation,
+    Alignment alignment,
+    Widget child,
+  );
+
+  static const types = [
+    BounceAnimationType(),
+    FadeAnimationType(),
+    ScaleAnimationType(),
+  ];
+
+  @override
+  String toString() => name;
+}
+
+class BounceAnimationType extends AnimationType {
+  const BounceAnimationType()
+      : super(
+          title: 'Bounce (default)',
+          name: 'Bounce',
+        );
+
+  @override
+  String buildCode() {
+    return '';
+  }
+
+  @override
+  Widget builder(
+    BuildContext context,
+    Animation<double> animation,
+    Alignment alignment,
+    Widget child,
+  ) {
+    return ToastificationTransition(
+      animation: animation,
+      alignment: alignment,
+      child: child,
+    );
+  }
+}
+
+class FadeAnimationType extends AnimationType {
+  const FadeAnimationType()
+      : super(
+          title: 'Fade in / out',
+          name: 'Fade',
+        );
+
+  @override
+  String buildCode() {
+    return '''(context, animation, alignment, child,) {
+      return FadeTransition(
+        opacity: animation,
+        child: child,
+      );
+    }''';
+  }
+
+  @override
+  Widget builder(
+    BuildContext context,
+    Animation<double> animation,
+    Alignment alignment,
+    Widget child,
+  ) {
+    return FadeTransition(
+      opacity: animation,
+      child: child,
+    );
+  }
+}
+
+class ScaleAnimationType extends AnimationType {
+  const ScaleAnimationType()
+      : super(
+          title: 'Slide in / out',
+          name: 'Scale',
+        );
+
+  @override
+  String buildCode() {
+    return '''(context, animation, alignment, child,) {
+      return ScaleTransition(
+        scale: animation,
+        child: child,
+      );
+    }''';
+  }
+
+  @override
+  Widget builder(
+    BuildContext context,
+    Animation<double> animation,
+    Alignment alignment,
+    Widget child,
+  ) {
+    return ScaleTransition(
+      scale: animation,
+      child: child,
+    );
+  }
+}
