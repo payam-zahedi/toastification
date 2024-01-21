9c2da50fec1613788922fc844db062643513e3d6
mahmoud <eslamimahmoud98@gmail.com>
2024-01-21 07:23:49 +0330

    feat: implemenet base widgets of the icon picker

---
+++ example/lib/src/core/views/widgets/picker/icon/icon.dart
@@ -0,0 +1,160 @@
+import 'package:example/src/core/views/widgets/bordered_container.dart';
+import 'package:example/src/core/views/widgets/picker/icon/icon_provider.dart';
+import 'package:example/src/features/home/controllers/toast_detail.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:iconsax_flutter/iconsax_flutter.dart';
+import 'package:toastification/toastification.dart';
+
+class IconPicker extends ConsumerStatefulWidget {
+  const IconPicker({super.key});
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() => _IconPickerState();
+}
+
+class _IconPickerState extends ConsumerState<IconPicker> {
+  final GlobalKey _menuKey = GlobalKey();
+
+  void openPopup() {
+    dynamic state = _menuKey.currentState;
+    state.showButtonMenu();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final type = ref.watch(
+      toastDetailControllerProvider.select((value) => value.type),
+    );
+    final style = ref.watch(
+      toastDetailControllerProvider.select((value) => value.style),
+    );
+
+    final iconColor = ref.watch(toastDetailControllerProvider).primaryColor;
+
+    final defaultStyle = switch (style) {
+      ToastificationStyle.minimal => MinimalStyle(type),
+      ToastificationStyle.fillColored => FilledStyle(type),
+      ToastificationStyle.flatColored => FlatColoredStyle(type),
+      ToastificationStyle.flat => FlatStyle(type),
+      ToastificationStyle.simple => SimpleStyle(type),
+    };
+
+    return Theme(
+      data: Theme.of(context).copyWith(
+        hoverColor: Colors.transparent,
+      ),
+      child: PopupMenuButton(
+        key: _menuKey,
+        tooltip: "",
+        clipBehavior: Clip.hardEdge,
+        position: PopupMenuPosition.under,
+        elevation: 0,
+        padding: EdgeInsets.zero,
+        enabled: true,
+        color: Theme.of(context).colorScheme.background,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10),
+          side: BorderSide(
+            color: Theme.of(context).colorScheme.outline,
+            width: 1.5,
+          ),
+        ),
+        child: BorderedContainer(
+          width: 120,
+          height: 48,
+          onTap: openPopup,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(
+                Iconsax.tick_circle_copy,
+                color: iconColor ?? defaultStyle.iconColor(context),
+              ),
+              const SizedBox(width: 8),
+              const Text('Icon'),
+              const SizedBox(width: 4),
+              const Icon(
+                Icons.keyboard_arrow_down,
+                size: 20,
+              ),
+            ],
+          ),
+        ),
+        itemBuilder: (context) => [
+          PopupMenuItem(
+            onTap: null,
+            enabled: false,
+            padding: EdgeInsets.zero,
+            child: Center(
+              child: IconPickerWidget(
+                iconColor: iconColor ?? defaultStyle.iconColor(context),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class IconPickerWidget extends ConsumerWidget {
+  const IconPickerWidget({super.key, required this.iconColor});
+
+  final Color iconColor;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final list = ref.watch(filteredListProvider);
+    final textTheme = Theme.of(context).textTheme;
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+      child: SizedBox(
+        width: 300,
+        height: 400,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Icons",
+              style: textTheme.bodyLarge?.copyWith(
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            TextFormField(
+              decoration: const InputDecoration(
+                label: Text("Search"),
+              ),
+              onChanged: (value) =>
+                  ref.read(searchedIconQuery.notifier).state = value,
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Expanded(
+              child: GridView.builder(
+                shrinkWrap: false,
+                gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+                  maxCrossAxisExtent: 30,
+                  crossAxisSpacing: 10,
+                  mainAxisSpacing: 10,
+                ),
+                itemCount: list.length,
+                itemBuilder: (context, index) {
+                  final item = list[index];
+
+                  return Icon(
+                    item.value,
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
