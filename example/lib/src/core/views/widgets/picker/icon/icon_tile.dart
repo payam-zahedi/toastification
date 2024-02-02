0ca22161f3cf615dcf3988ae91e7335fa8c60401
mahmoud <eslamimahmoud98@gmail.com>
2024-02-02 09:21:20 +0330

    feat: change icon value in provider

--- example/lib/src/core/views/widgets/picker/icon/icon_tile.dart
+++ example/lib/src/core/views/widgets/picker/icon/icon_tile.dart
@@ -1,3 +1,4 @@
+import 'package:example/src/features/home/controllers/toast_detail.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -13,6 +14,9 @@ class _IconTileState extends ConsumerState<IconTile> {
   bool hovered = false;
 
   void chooseIcon() {
+    ref.read(toastDetailControllerProvider.notifier).changeIcon(
+          Icon(widget.item.value),
+        );
     Navigator.pop(context);
   }
 
