389dfd2da35bd05e778926f6aefad8b7624a89ed
Antônio A E Martins <81169982+AntonioAEMartins@users.noreply.github.com>
2024-10-04 12:35:41 -0300

    add IconModelSerialization extesion to IconModel

--- example/lib/src/features/home/views/ui_states/icon_model.dart
+++ example/lib/src/features/home/views/ui_states/icon_model.dart
@@ -8,5 +8,48 @@ class IconModel with _$IconModel {
   factory IconModel({
     required String name,
     required IconData iconData,
+    @Default(Colors.white) Color color,
   }) = _IconModel;
+}
+
+extension IconModelSerialization on IconModel {
+  Map<String, dynamic> toMap() {
+    return {
+      'name': name,
+      'code_point': iconData.codePoint,
+      'font_family': iconData.fontFamily,
+      'font_package': iconData.fontPackage,
+      'match_text_direction': iconData.matchTextDirection,
+      'color': _colorToHex(color),
+    };
+  }
+
+  static IconModel fromMap(Map<String, dynamic> map) {
+    final int codePoint = map['code_point'];
+    final String? fontFamily = map['font_family'];
+    final String? fontPackage = map['font_package'];
+    final bool matchTextDirection = map['match_text_direction'] ?? false;
+    final Color color = _colorFromHex(map['color']) ?? Colors.white;
+
+    return IconModel(
+      name: map['name'] ?? 'default_name',
+      iconData: IconData(
+        codePoint,
+        fontFamily: fontFamily,
+        fontPackage: fontPackage,
+        matchTextDirection: matchTextDirection,
+      ),
+      color: color,
+    );
+  }
+
+  // Helper methods for color conversion
+  static String _colorToHex(Color color) {
+    return '#${color.value.toRadixString(16).padLeft(8, '0')}';
+  }
+
+  static Color? _colorFromHex(String? hex) {
+    if (hex == null) return null;
+    return Color(int.parse(hex.replaceFirst('#', '0xff')));
+  }
 }
